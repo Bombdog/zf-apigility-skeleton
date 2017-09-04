9b1190db24d559356bc8f98c399710942f5ca3be
Andy Harrison <andy@growsoft.co.uk>
2017-09-04 15:10:03 +0100

    Create two users in fixture

--- module/Fixture/src/V1/Rpc/Apply/ApplyController.php
+++ module/Fixture/src/V1/Rpc/Apply/ApplyController.php
@@ -2,33 +2,66 @@
 namespace Fixture\V1\Rpc\Apply;
 
 use Application\Api\Response\ApiSuccessResponse;
+use Doctrine\ODM\MongoDB\DocumentManager;
 use Entity\Document\User;
 use Zend\Mvc\Controller\AbstractActionController;
 
 class ApplyController extends AbstractActionController
 {
+    /**
+     * @var DocumentManager
+     */
+    private $dm;
+
+    /**
+     * Set dm
+     * @param DocumentManager $dm
+     */
+    public function setDocumentManager($dm)
+    {
+        $this->dm = $dm;
+    }
+
+    /**
+     * Apply some fixtures
+     * @return ApiSuccessResponse
+     */
     public function applyAction()
     {
+        $this->purge();
+
         # User "bob" with password "canread1"
-        $user = new User();
-        $user->setEmail('bob@blah.com')
+        $user1 = new User();
+        $user1->setEmail('bob@blah.com')
             ->setEmailVerified(true)
             ->setFirstName('Bob')
             ->setLastName('McTest')
             ->setEnabled(true)
             ->setScope('articles:read')
             ->setPassword('canread1');
+        $this->dm->persist($user1);
 
         # User "donald" with password "canwrite1"
-        $user = new User();
-        $user->setEmail('donald@blah.com')
+        $user2 = new User();
+        $user2->setEmail('donald@blah.com')
             ->setEmailVerified(true)
             ->setFirstName('Donald')
             ->setLastName('McTest')
             ->setEnabled(true)
             ->setScope('articles:write_all')
             ->setPassword('canwrite1');
+        $this->dm->persist($user2);
+        $this->dm->flush();
 
         return new ApiSuccessResponse(204);
+    }
+
+    /**
+     * Purge everything
+     */
+    private function purge()
+    {
+        # drop all mongo collections
+        $this->dm->getSchemaManager()->dropCollections();
     }
 }
--- module/Fixture/src/V1/Rpc/Apply/ApplyControllerFactory.php
+++ module/Fixture/src/V1/Rpc/Apply/ApplyControllerFactory.php
@@ -1,10 +1,16 @@
 <?php
 namespace Fixture\V1\Rpc\Apply;
 
+use Zend\ServiceManager\ServiceManager;
+
 class ApplyControllerFactory
 {
-    public function __invoke($controllers)
+    public function __invoke(ServiceManager $controllers)
     {
-        return new ApplyController();
+        $dm = $controllers->get('doctrine.documentmanager.odm_default');
+        $controller =  new ApplyController();
+        $controller->setDocumentManager($dm);
+
+        return $controller;
     }
 }
